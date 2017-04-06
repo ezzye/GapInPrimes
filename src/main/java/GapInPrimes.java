eb77a8deb94fc3f6b0ea31803d0edb734486d256
ezzye <ezzy.elliott@gmail.com>
2017-04-06 09:41:20 +0100

    First commit

---
+++ src/main/java/GapInPrimes.java
@@ -0,0 +1,43 @@
+/**
+ * Created by ellioe03 on 05/04/2017.
+ */
+class GapInPrimes {
+
+    public static long[] gap(int g, long m, long n) {
+        long first;
+        long second;
+        first = nextPrimeNumberIncluding(m);
+        long[] result = new long[2];
+
+        while(1==1) {
+            second = nextPrimeNumberIncluding(first + g);
+            if(second-first==g) {
+                result[0]=first;
+                result[1]=second;
+                return result;
+            }
+            first++;
+            first = nextPrimeNumberIncluding(first);
+            //second less first less than or equal to two return null
+        }
+
+    }
+
+    public static long nextPrimeNumberIncluding(long n) {
+        long nextPrime = n;
+        while(1==1) {
+            boolean isPrime = true;
+            for (long i = 2;i < nextPrime;i++) {
+                if(nextPrime%i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if(isPrime) {
+                return nextPrime;
+            }
+            nextPrime++;
+        }
+
+    }
+}
---
+++ src/test/java/GapInPrimesTest.java
@@ -0,0 +1,68 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ellioe03 on 05/04/2017.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+public class GapInPrimesTest {
+
+    @Test
+    public void shouldWorkForGapOfTwoLowNumbers() {
+        assertEquals("[3, 5]", Arrays.toString(GapInPrimes.gap(2,3,50)));
+    }
+
+    @Test
+    public void shouldWorkForGapOfTwoForNotFirstNumber() {
+        assertEquals("[5, 7]", Arrays.toString(GapInPrimes.gap(2,4,50)));
+    }
+
+    @Test
+    public void shouldWorkForAnyGapForNumber() {
+        assertEquals("[7, 11]", Arrays.toString(GapInPrimes.gap(4,7,50)));
+    }
+
+    @Test
+    public void shouldWorkForAnyGapForNotFirstNumber() {
+        assertEquals("[7, 11]", Arrays.toString(GapInPrimes.gap(4,5,50)));
+    }
+
+    @Test
+    public void shouldWorkForGapOfTwoBigNumbers() {
+        assertEquals("[101, 103]", Arrays.toString(GapInPrimes.gap(2,100,110)));
+    }
+
+    @Test
+    public void shouldWorkForGapOfTwoBigNumbersGapFour() {
+        assertEquals("[103, 107]", Arrays.toString(GapInPrimes.gap(4,100,110)));
+    }
+
+    @Test
+    public void shouldGiveNullWhereNoPairFound() {
+        assertEquals(null, GapInPrimes.gap(6,100,110));
+    }
+
+    @Test
+    public void NextPrimeShouldWorkForLowPrimeNumbers() {
+        assertEquals(5,GapInPrimes.nextPrimeNumberIncluding(4));
+    }
+
+    @Test
+    public void NextPrimeShouldWorkForOneAndTwo() {
+        assertEquals(1,GapInPrimes.nextPrimeNumberIncluding(1));
+        assertEquals(2,GapInPrimes.nextPrimeNumberIncluding(2));
+        assertEquals(3,GapInPrimes.nextPrimeNumberIncluding(3));
+        assertEquals(5,GapInPrimes.nextPrimeNumberIncluding(5));
+
+
+
+    }
+
+
+}
